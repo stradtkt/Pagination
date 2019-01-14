820c5d7470e97aa7dfbf5fb99d8a88618a0cda6e
Kevin Stradtman <stradtkt22@gmail.com>
2019-01-14 17:20:47 -0500

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,92 @@
+/******************************************
+Treehouse Techdegree:
+FSJS project 2 - List Filter and Pagination
+******************************************/
+(function() {
+   'use strict';
+
+   var studentItems = $('.student-item');
+   var pagination = "<div class='pagination'><ul></ul></div>";
+   var studentList = pages(studentItems);
+
+
+////////////////////////////////////////////////////////
+//Search bar
+////////////////////////////////////////////////////////
+
+    var studentSearch = '<div class="student-search"><input id="searchName" placeholder="Search Students"/><button id="submit">Search</button></div>';
+    $('.page-header.cf').append(studentSearch);
+    function search() {
+       var searchItem = $('#searchName').val().toLowerCase().trim();
+       var filterStudents = studentItems.filter(function(i) {
+          var email = $(this).find('.email').text();
+          var name = $(this).find('h3').text();
+          if (name.indexOf(searchItem) > - 1 || email.indexOf(searchItem) > - 1) {
+             return true;
+          } else {
+             return false;
+          }
+       });
+
+       if(filterStudents.length === 0) {
+          $('.page-header h2').text("Sorry there are no results");
+       } else {
+          $('.page-header h2').text("Students searched");
+       }
+       var students = pages(filterStudents);
+       $('.pagination').remove();
+       if(filterStudents.length >= 10) {
+          appendButtons(students);
+       }
+       showPages(0, students);
+    }
+/**************************************************************************************************************************************************************************************************************** */
+
+////////////////////////////////////////////////////
+//Pagination
+////////////////////////////////////////////////////
+function pages(list) {
+   var oldList = list.slice();
+   var pagesArray = [];
+   while(oldList.length) {
+      pagesArray.push(oldList.splice(0, 10));
+   }
+   return pagesArray;
+}
+
+function showPages(pageNumber, pageList) {
+   $('.student-list li').hide();
+   $.each(pageList, function(index, page) {
+      if(pageNumber === index) {
+         $.each(page, function(i, listItem) {
+            $(listItem).fadeIn('fast');
+         });
+      }
+   });
+}
+
+function appendButtons(pageList) {
+   $('.page').append(pagination);
+   var numPages = pageList.length;
+   for(var i = 1; i <= numPages; i++) {
+      var buttons = '<li><a href="#">'+ i +'</a></li>';
+      $('.pagination ul').append(buttons);
+   }
+   $('.pagination ul li a').first().addClass('active');
+   $('.pagination ul li a').on('click', function(event) {
+      var pageSelection = parseInt($(this)[0].text) - 1;
+      showPages(pageSelection, pageList);
+      $('.pagination ul li a').removeClass();
+      $(this).addClass('active');
+      event.preventDefault();
+   });
+}
+
+//calling the function appendBtns which provides the correct amount of buttons for the amount of items that are in the studentList
+appendButtons(studentList);
+//calling the showPages function starting at the zero index and then displays the next 10 results from the studentList
+showPages(0, studentList);
+//calls the search function when the button is clicked
+$('.student-search').find('button').on('click', search);
+$('.student-search').find('input').keyup(search);
+})();
