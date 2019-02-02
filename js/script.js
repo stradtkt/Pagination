0651c5159d8c6ab1e1c860bb174372fda0ee8ca5
Kevin Stradtman <stradtkt22@gmail.com>
2019-02-01 22:39:14 -0500

    Added the comments to the finished project

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ FSJS project 2 - List Filter and Pagination
 ******************************************/
 (function() {
    'use strict';
-
+   // global variables 
    var studentItems = $('.student-item');
    var pagination = "<div class='pagination'><ul></ul></div>";
    var studentList = pages(studentItems);
@@ -17,8 +17,10 @@ FSJS project 2 - List Filter and Pagination
     var studentSearch = '<div class="student-search"><input id="searchName" placeholder="Search Students"/><button id="submit">Search</button></div>';
     $('.page-header.cf').append(studentSearch);
     function search() {
+       // get the val of the search item then lowercase the result then trim it to remove whitespace
        var searchItem = $('#searchName').val().toLowerCase().trim();
        var filterStudents = studentItems.filter(function(i) {
+          // find the email or name from the list of students
           var email = $(this).find('.email').text();
           var name = $(this).find('h3').text();
           if (name.indexOf(searchItem) > - 1 || email.indexOf(searchItem) > - 1) {
@@ -45,6 +47,7 @@ FSJS project 2 - List Filter and Pagination
 ////////////////////////////////////////////////////
 //Pagination
 ////////////////////////////////////////////////////
+// the pages for the application
 function pages(list) {
    var oldList = list.slice();
    var pagesArray = [];
@@ -53,7 +56,7 @@ function pages(list) {
    }
    return pagesArray;
 }
-
+// showing the correct page out of the pagelist
 function showPages(pageNumber, pageList) {
    $('.student-list li').hide();
    $.each(pageList, function(index, page) {
@@ -66,16 +69,20 @@ function showPages(pageNumber, pageList) {
 }
 
 function appendButtons(pageList) {
+   // append the list
    $('.page').append(pagination);
    var numPages = pageList.length;
    for(var i = 1; i <= numPages; i++) {
       var buttons = '<li><a href="#">'+ i +'</a></li>';
+      // append the list item that are the buttons for the pagination
       $('.pagination ul').append(buttons);
    }
+   // add an active class to the page the user is on
    $('.pagination ul li a').first().addClass('active');
    $('.pagination ul li a').on('click', function(event) {
       var pageSelection = parseInt($(this)[0].text) - 1;
       showPages(pageSelection, pageList);
+      // onclick removes the active class from the previous and then sets it to the new item
       $('.pagination ul li a').removeClass();
       $(this).addClass('active');
       event.preventDefault();
